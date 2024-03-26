406cf153fd7903653d45e22966308137c63e552b
Anup-Nayak <gpnayak1930@gmail.com>
2024-03-26 23:21:50 +0530

    final

--- secd.ml
+++ secd.ml
@@ -1,6 +1,4 @@
-open Stdlib
-
-type exp = Num of int | Bl of bool
+type exp = N of int | B of bool
   | V of string
   | Plus of exp * exp | Times of exp * exp
   | And of exp * exp | Or of exp * exp | Not of exp
@@ -23,8 +21,8 @@ type opcode = LDN of int | LDB of bool | LOOKUP of string
 
   
   let rec compile e = match e with
-  Num n -> [ LDN n ]
-  | Bl b -> [ LDB b ] 
+  N n -> [ LDN n ]
+  | B b -> [ LDB b ] 
   | V x -> [LOOKUP x] 
   | Plus (e1, e2) -> (compile e1) @ (compile e2) @ [PLUS]
   | Times (e1, e2) -> (compile e1) @ (compile e2) @ [TIMES]
@@ -35,14 +33,14 @@ type opcode = LDN of int | LDB of bool | LOOKUP of string
   | Gt(e1, e2) -> (compile e1) @ (compile e2) @ [GT]
   | IfTE(e0, e1, e2) -> (compile e0) @ [COND(compile e1, compile e2)]
   | Pair (e1, e2) -> (compile e1) @ (compile e2) @ [PAIR]
-  | Fst e -> (compile e) @ [FST]
-  | Snd e -> (compile e) @ [SND]
-  | Abs (x,e) -> [ MKCLOS(x, compile(e)@[RET]) ]
+  | Fst e1 -> (compile e1) @ [FST]
+  | Snd e1 -> (compile e1) @ [SND]
+  | Abs (x,e1) -> [ MKCLOS(x, compile(e1)@[RET]) ]
   | App (e1,e2) -> compile(e1)@compile(e2)@[APP]
 ;;
 
 type values = N of int | B of bool | P of values * values |  Closure of string * opcode list * ((string*values) list);; 
-type env = (string*values) list
+type env = (string*values) list;;
 
 exception Stuck of env * values list * opcode list;;
 
@@ -78,114 +76,37 @@ let rec stkmc g s c d = match s, c, d  with
   | s, MKCLOS(x',c')::c'',d -> 
     let closure = Closure (x', c', g) in
     stkmc g (closure :: s) c'' d
-  | a::Closure (x,c',g')::s' ,APP::c'', d -> 
+  | a::Closure (x,c',g')::s', APP::c'', d -> 
     let g'' =  update_var x a g' in
     stkmc g'' [] c' ((s',g,c'')::d)
   | a::s', RET :: c' , (s'',g',c'')::d -> stkmc g' (a::s'') c'' d
   | _, _,_ -> raise (Stuck (g, s, c))
   ;;
 
-let rec string_of_opcode opcode = match opcode with
-  | LDN n -> "LDN " ^ string_of_int n
-  | LDB b -> "LDB " ^ string_of_bool b
-  | LOOKUP x -> "LOOKUP " ^ x
-  | PLUS -> "PLUS"
-  | TIMES -> "TIMES"
-  | AND -> "AND"
-  | OR -> "OR"
-  | NOT -> "NOT"
-  | EQ -> "EQ"
-  | GT -> "GT"
-  | COND (c1, c2) -> "COND [" ^ (String.concat "; " (List.map string_of_opcode c1)) ^ "], [" ^ (String.concat "; " (List.map string_of_opcode c2)) ^ "]"
-  | PAIR -> "PAIR"
-  | FST -> "FST"
-  | SND -> "SND"
-  | RET -> "RET"
-  | APP -> "APP"
-  | MKCLOS(a,b) -> "MKCLOS [" ^ a ^ "], [" ^ (String.concat "; " (List.map string_of_opcode b)) ^ "]"
-;;
-
-
-(* checking opcodes *)
-let print_opcodes opcodes =
-  let rec print_opcodes_helper = function
-    | [] -> ()
-    | opcode :: rest ->
-        print_endline (string_of_opcode opcode);
-        print_opcodes_helper rest
-  in
-  print_opcodes_helper opcodes;;
-
-(* checking compile *)
-let test_cases_compile = [
-  (* Test addition *)
-  (Num 5, [LDN 5]);
-
-  (* Test boolean *)
-  (Bl true, [LDB true]);
-
-  (* Test variable *)
-  (V "x", [LOOKUP "x"]);
-
-  (* Test addition of two numbers *)
-  (Plus (Num 3, Num 4), [LDN 3; LDN 4; PLUS]);
-
-  (* Test if-then-else *)
-  (IfTE (Bl true, Num 1, Num 2), [LDB true; COND ([LDN 1], [LDN 2])]);
-
-  (* Test pair *)
-  (Pair (Num 5, Bl true), [LDN 5; LDB true; PAIR]);
-
-  (* Test projection *)
-  (Fst (Pair (Num 5, Bl true)), [LDN 5; LDB true; PAIR; FST]);
-
-  (* Test multiple operations *)
-  (Plus (Num 3, Times (Num 4, Num 2)), [LDN 3; LDN 4; LDN 2; TIMES; PLUS]);
-]
-
-
-let test_compile () =
-  let rec run_tests = function
-    | [] -> print_endline "All test cases passed!"
-    | (expr, expected_opcodes) :: rest ->
-        let generated_opcodes = compile expr in
-        if generated_opcodes = expected_opcodes then
-          (print_endline "Test passed!";
-           run_tests rest)
-        else
-          (print_endline "Test failed!";
-           print_endline ("Expected: " ^ (String.concat "; " (List.map string_of_opcode expected_opcodes)));
-           print_endline ("Generated: " ^ (String.concat "; " (List.map string_of_opcode generated_opcodes))))
-  in
-  run_tests test_cases_compile;;
-
-(* Run test cases *)
-(* test_compile ();; *)
-
 
 let test_cases = [
   (* Basic arithmetic and comparison *)
   ([N 1],[],[],[],N 1);
-  ([],[],compile(Plus(Num 3,Num 4)),[], N 7);
-  ([],[],compile(Times(Num 8,Num 9)),[], N 72);
-  ([], [], compile(Plus(Num 3, Num 4)), [], N 7);
-  ([], [], compile(Times(Num 8, Num 9)), [], N 72);
-  ([], [], compile(Eq(Num 5, Num 5)), [], B true);
-  ([], [], compile(Eq(Num 5, Num 6)), [], B false);
-  ([], [], compile(Gt(Num 7, Num 4)), [], B true);
-  ([], [], compile(Gt(Num 3, Num 5)), [], B false);
+  ([],[],compile(Plus(N 3,N 4)),[], N 7);
+  ([],[],compile(Times(N 8,N 9)),[], N 72);
+  ([], [], compile(Plus(N 3, N 4)), [], N 7);
+  ([], [], compile(Times(N 8, N 9)), [], N 72);
+  ([], [], compile(Eq(N 5, N 5)), [], B true);
+  ([], [], compile(Eq(N 5, N 6)), [], B false);
+  ([], [], compile(Gt(N 7, N 4)), [], B true);
+  ([], [], compile(Gt(N 3, N 5)), [], B false);
 
   (* Boolean expressions *)
-  ([], [], compile(And(Bl true, Bl true)), [], B true);
-  ([], [], compile(And(Bl true, Bl false)), [], B false);
-  ([], [], compile(Or(Bl false, Bl false)), [], B false);
-  ([], [], compile(Or(Bl false, Bl true)), [], B true);
-  ([], [], compile(Not(Bl true)), [], B false);
-  ([], [], compile(Not(Bl false)), [], B true);
+  ([], [], compile(And(B true, B true)), [], B true);
+  ([], [], compile(And(B true, B false)), [], B false);
+  ([], [], compile(Or(B false, B false)), [], B false);
+  ([], [], compile(Or(B false, B true)), [], B true);
+  ([], [], compile(Not(B true)), [], B false);
+  ([], [], compile(Not(B false)), [], B true);
 
   (* Conditional expressions *)
-  ([], [], compile(IfTE(Bl true, Num 10, Num 20)), [], N 10);
-  ([], [], compile(IfTE(Bl false, Num 10, Num 20)), [], N 20);
+  ([], [], compile(IfTE(B true, N 10, N 20)), [], N 10);
+  ([], [], compile(IfTE(B false, N 10, N 20)), [], N 20);
 
   (* Variables *)
   ([],[("x", N 6);("y",N 7)], compile(Plus(V "x",V "y")),[], N 13);
@@ -197,9 +118,20 @@ let test_cases = [
   ([],["pair", P(N 42, B true)],compile(Snd(V "pair")),[],B true);
 
   (* Function abstraction and application *)
-  
+  ([],[],[MKCLOS("x",[])],[],Closure("x",[],[]));
+  ([],[],[MKCLOS("x",[PLUS;EQ])],[],Closure("x",[PLUS;EQ],[]));
+  ([],[("y", N 42)],[MKCLOS("x",[PLUS;EQ])],[],Closure("x",[PLUS;EQ],[("y", N 42)]));
+  ([],[],compile(App(Abs("x",Plus(V "x", N 20)),N 5)),[],N 25);
+
+  (* Nested applications *)
+  ([], [], compile(App(Abs("y", App(Abs("x", Plus(V "x", V "y")), N 3)), N 2)), [], N 5);
+
+  (* Variable capture in abstraction and scoping *)
+  ([], [("x", N 22)], compile(App(Abs("x", Plus(V "x", N 20)), V "x")), [], N 42);
+  ([], [("x", N 22);("y", N 2)], compile(App(Abs("x", Times(Plus(V "x", N 20),V "y")), V "x")), [], N 84);
 
   (* Error cases  *)
+  (* change any of the above input/output *)
 
 ]
 
